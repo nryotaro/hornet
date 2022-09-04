b2ecd26bc15a9a57a396453e4cfd5f5cef6b28a3
Nakamura, Ryotaro <nakamura.ryotaro.kzs@gmail.com>
2022-09-04 22:10:20 +0900

    add rrshift lrshift

--- hornet/node.py
+++ hornet/node.py
@@ -13,7 +13,9 @@ class Node:
         self.digraph: _graphviz.Digraph = _state.get_digraph()
         self.digraph.node(self.identity, None, attrs)
 
-    def __lt__(self, other: "Node" | typing.Iterable["Node"]) -> "Node":
+    def __lshift__(
+        self, other: "Node" | typing.Iterable["Node"] | typing.Iterator["Node"]
+    ) -> "Node":
         """Add an edge from `other` to `self`."""
         nodes = other
         if hasattr(other, "__iter__"):
@@ -28,7 +30,17 @@ class Node:
 
         return result
 
-    def __gt__(self, other: "Node") -> "Node":
+    def __rlshift__(
+        self, others: typing.Iterable["Node"] | typing.Iterator["Node"]
+    ) -> "Node":
+        """Add an edge from `self` to `others`."""
+        # [nodes] << node
+        self >> others
+        return self
+
+    def __rshift__(
+        self, other: "Node" | typing.Iterable["Node"] | typing.Iterator["Node"]
+    ) -> "Node":
         """Add an edge from `self` to `other`."""
         nodes = other
         if hasattr(other, "__iter__"):
@@ -44,6 +56,14 @@ class Node:
                 node.identity,
             )
         return result
+
+    def __rrshift__(
+        self, others: typing.Iterator["Node"] | typing.Iterable["Node"]
+    ) -> "Node":
+        """Add edges from `others` to `self`."""
+        # [others] >> self
+        self << others
+        return self
 
     def __sub__(self, other: "Node") -> "Node":
         """Implement - ."""
